87d279e27e6f206e6bad938363769cb45555f483
Liam Fisher <liam.fisher24@houghton.edu>
2021-10-02 12:18:44 -0300

    Added final code.

---
+++ stopLight.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+import time
+import threading
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(19, GPIO.IN)
+
+Lights_On = 1
+
+def onOff():
+    global Lights_On
+    while True:
+        if GPIO.input(19):
+            Lights_On *= -1
+            time.sleep(1.5)
+        time.sleep(0.005)
+
+def lights():
+    global Lights_On
+    while True:
+        if Lights_On == 1:
+            GPIO.output(20, False)
+            GPIO.output(21, False)
+            GPIO.output(18, True)
+            time.sleep(3)
+            GPIO.output(18, False)
+            GPIO.output(20, True)
+            time.sleep(1)
+            GPIO.output(20, False)
+            GPIO.output(21, True)
+            time.sleep(4)
+        else: 
+            GPIO.output(18, False)
+            GPIO.output(21, False)
+            GPIO.output(20, True)
+            time.sleep(2)
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target = onOff)
+    t2 = threading.Thread(target = lights)
+    t1.start()
+    t2.start()
+
+GPIO.cleanup()
